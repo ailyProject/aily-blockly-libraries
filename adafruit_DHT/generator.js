1d40fef26705a83926b825e39b88daa4a355b4e8
i3water <121024123@qq.com>
2025-09-26 12:12:01 +0800

    更新DHT库版本

--- adafruit_DHT/generator.js
+++ adafruit_DHT/generator.js
@@ -1,93 +1,85 @@
-// 跟踪已初始化的DHT传感器
-// 在每次代码生成时重置跟踪状态
-if (!Arduino.dhtCodeGeneration) {
-  Arduino.dhtCodeGeneration = true;
-
-  // 保存原始的代码生成方法
-  const originalWorkspaceToCode = Arduino.workspaceToCode;
-
-  // 重写代码生成方法以在开始时重置DHT跟踪
-  Arduino.workspaceToCode = function(workspace) {
-    // 重置DHT初始化跟踪
-    Arduino.initializedDHTSensors = new Set();
-    Arduino.addedDHTInitCode = new Set();
-
-    // 调用原始方法
-    return originalWorkspaceToCode ? originalWorkspaceToCode.call(this, workspace) : '';
-  };
-}
-
-if (!Arduino.initializedDHTSensors) {
-  Arduino.initializedDHTSensors = new Set();
-}
-
-if (!Arduino.addedDHTInitCode) {
-  Arduino.addedDHTInitCode = new Set();
-}
-
-// 辅助函数，确保DHT传感器已被初始化
-function ensureDHTInit(pin, dht_type, generator) {
-  // 创建唯一的DHT标识符
-  var dhtId = pin + '_' + dht_type.toLowerCase();
-  var dhtObjName = 'dht_' + dhtId;
-
-  // 检查这个DHT传感器是否已经添加过初始化代码
-  if (!Arduino.addedDHTInitCode.has(dhtId)) {
-    // 添加DHT库
-    generator.addLibrary('DHT_include', '#include <DHT.h>');
-
-    // 创建DHT对象
-    var dhtDef = 'DHT ' + dhtObjName + '(' + pin + ', ' + dht_type + ');';
-    generator.addVariable(dhtObjName + '_def', dhtDef);
-
-    // 在setup中初始化DHT
-    generator.addSetupBegin(dhtObjName + '_begin', dhtObjName + '.begin();');
-
-    // 标记为已添加初始化代码
-    Arduino.addedDHTInitCode.add(dhtId);
-    Arduino.initializedDHTSensors.add(dhtId);
+// 通用库管理函数，确保不重复添加库
+function ensureLibrary(generator, libraryKey, libraryCode) {
+  if (!generator.libraries_) {
+    generator.libraries_ = {};
   }
+  if (!generator.libraries_[libraryKey]) {
+    generator.addLibrary(libraryKey, libraryCode);
+  }
+}
 
-  return dhtObjName;
+function ensureDHTLibrary(generator) {
+  ensureLibrary(generator, 'DHT_include', '#include <DHT.h>');
 }
 
 Arduino.forBlock['dht_init'] = function (block, generator) {
-    var dht_type = block.getFieldValue('TYPE');
-    var pin = block.getFieldValue('PIN');
+  // 监听VAR输入值的变化，自动重命名Blockly变量
+  if (!block._dhtVarMonitorAttached) {
+    block._dhtVarMonitorAttached = true;
+    block._dhtVarLastName = block.getFieldValue('VAR') || 'dht';
+    const varField = block.getField('VAR');
+    if (varField && typeof varField.setValidator === 'function') {
+      varField.setValidator(function(newName) {
+        const workspace = block.workspace || (typeof Blockly !== 'undefined' && Blockly.getMainWorkspace && Blockly.getMainWorkspace());
+        const oldName = block._dhtVarLastName;
+        if (workspace && newName && newName !== oldName) {
+          renameVariableInBlockly(block, oldName, newName, 'DHT');
+          block._dhtVarLastName = newName;
+        }
+        return newName;
+      });
+    }
+  }
 
-    // 使用ensureDHTInit确保DHT传感器初始化
-    ensureDHTInit(pin, dht_type, generator);
+  var varName = block.getFieldValue('VAR') || 'dht';
+  var dht_type = block.getFieldValue('TYPE');
+  var pin = block.getFieldValue('PIN');
 
-    return '';
+  // 注册Blockly变量，类型为DHT，同名变量只注册一次
+  registerVariableToBlockly(varName, 'DHT');
+
+  // 确保DHT库已添加
+  ensureDHTLibrary(generator);
+
+  // 添加DHT对象定义，使用用户指定的变量名
+  var dhtDef = 'DHT ' + varName + '(' + pin + ', ' + dht_type + ');';
+  generator.addVariable(varName, dhtDef);
+
+  generator.sensorVarName = varName;
+
+  // 在setup中初始化DHT对象
+  generator.addSetupBegin(varName + '_begin', varName + '.begin();');
+
+  // 使用ensureDHTInit确保DHT传感器初始化
+  // ensureDHTInit(pin, dht_type, generator);
+
+  return '';
 };
 
 Arduino.forBlock['dht_read_temperature'] = function (block, generator) {
-    var dht_type = block.getFieldValue('TYPE');
-    var pin = block.getFieldValue('PIN');
+  const varField = block.getField('VAR');
+  const varName = varField ? varField.getText() : 'dht';
 
-    // 使用ensureDHTInit确保DHT传感器初始化并获取对象名
-    var dhtObjName = ensureDHTInit(pin, dht_type, generator);
-
-    return [dhtObjName + '.readTemperature()', Arduino.ORDER_ATOMIC];
+  // 使用ensureDHTInit确保DHT传感器初始化并获取对象名
+  // 直接使用块中的变量名作为对象名
+  return [varName + '.readTemperature()', Arduino.ORDER_ATOMIC];
 };
 
 Arduino.forBlock['dht_read_humidity'] = function (block, generator) {
-    var dht_type = block.getFieldValue('TYPE');
-    var pin = block.getFieldValue('PIN');
+  const varField = block.getField('VAR');
+  const varName = varField ? varField.getText() : 'dht';
 
-    // 使用ensureDHTInit确保DHT传感器初始化并获取对象名
-    var dhtObjName = ensureDHTInit(pin, dht_type, generator);
-
-    return [dhtObjName + '.readHumidity()', Arduino.ORDER_ATOMIC];
+  // 使用ensureDHTInit确保DHT传感器初始化并获取对象名
+  // 直接使用块中的变量名作为对象名
+  return [varName + '.readHumidity()', Arduino.ORDER_ATOMIC];
 };
 
 Arduino.forBlock['dht_read_success'] = function (block, generator) {
-    var dht_type = block.getFieldValue('TYPE');
-    var pin = block.getFieldValue('PIN');
+  const varField = block.getField('VAR');
+  const varName = varField ? varField.getText() : 'dht';
 
-    // 使用ensureDHTInit确保DHT传感器初始化并获取对象名
-    var dhtObjName = ensureDHTInit(pin, dht_type, generator);
-
-    return ['!isnan(' + dhtObjName + '.readTemperature()) && !isnan(' + dhtObjName + '.readHumidity())', Arduino.ORDER_LOGICAL_AND];
+  // 使用ensureDHTInit确保DHT传感器初始化并获取对象名
+  // 直接使用块中的变量名作为对象名
+  return ['!isnan(' + varName + '.readTemperature()) && !isnan(' + varName + '.readHumidity())', Arduino.ORDER_LOGICAL_AND];
 };
 
