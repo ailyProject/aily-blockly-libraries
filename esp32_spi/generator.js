3f4d9c8ba59566c88e0aa5e446df2b41f60fd918
i3water <121024123@qq.com>
2025-11-20 17:29:49 +0800

    更新

--- esp32_spi/generator.js
+++ esp32_spi/generator.js
@@ -1,10 +1,44 @@
 'use strict';
 
-Arduino.forBlock['esp32_spi_create'] = function(block, generator) {
+// Arduino.forBlock['esp32_spi_create'] = function(block, generator) {
+//   // 设置变量重命名监听
+//   if (!block._spiVarMonitorAttached) {
+//     block._spiVarMonitorAttached = true;
+//     block._spiVarLastName = block.getFieldValue('VAR') || 'SPI';
+//     const varField = block.getField('VAR');
+//     if (varField && typeof varField.setValidator === 'function') {
+//       varField.setValidator(function(newName) {
+//         const workspace = block.workspace || (typeof Blockly !== 'undefined' && Blockly.getMainWorkspace && Blockly.getMainWorkspace());
+//         const oldName = block._spiVarLastName;
+//         if (workspace && newName && newName !== oldName) {
+//           renameVariableInBlockly(block, oldName, newName, 'SPIClass');
+//           block._spiVarLastName = newName;
+//         }
+//         return newName;
+//       });
+//     }
+//   }
+
+//   const varName = block.getFieldValue('VAR') || 'SPI';
+//   const bus = block.getFieldValue('BUS') || 'VSPI';
+
+//   // 添加库引用
+//   generator.addLibrary('SPI', '#include <SPI.h>');
+  
+//   // 注册变量到Blockly系统
+//   registerVariableToBlockly(varName, 'SPIClass');
+  
+//   // 生成变量声明
+//   generator.addVariable(varName, 'SPIClass *' + varName + ' = new SPIClass(' + bus + ');');
+
+//   return '';
+// };
+
+Arduino.forBlock['esp32_spi_begin'] = function(block, generator) {
   // 设置变量重命名监听
   if (!block._spiVarMonitorAttached) {
     block._spiVarMonitorAttached = true;
-    block._spiVarLastName = block.getFieldValue('VAR') || 'spi';
+    block._spiVarLastName = block.getFieldValue('VAR') || 'SPI';
     const varField = block.getField('VAR');
     if (varField && typeof varField.setValidator === 'function') {
       varField.setValidator(function(newName) {
@@ -19,35 +53,45 @@ Arduino.forBlock['esp32_spi_create'] = function(block, generator) {
     }
   }
 
-  const varName = block.getFieldValue('VAR') || 'spi';
-  const bus = block.getFieldValue('BUS') || 'VSPI';
+  const varName = block.getFieldValue('VAR') || 'SPI';
+  const bus = block.getFieldValue('BUS') || 'HSPI';
 
-  // 添加库引用
   generator.addLibrary('SPI', '#include <SPI.h>');
-  
+
   // 注册变量到Blockly系统
   registerVariableToBlockly(varName, 'SPIClass');
   
-  // 生成变量声明
-  generator.addVariable(varName, 'SPIClass *' + varName + ' = new SPIClass(' + bus + ');');
+  if (bus === 'VSPI') {
+    generator.addVariable(varName, 'SPIClass ' + varName + '(VSPI);');
+  }
 
-  return '';
-};
-
-Arduino.forBlock['esp32_spi_begin'] = function(block, generator) {
-  const varField = block.getField('VAR');
-  const varName = varField ? varField.getText() : 'spi';
-
-  generator.addLibrary('SPI', '#include <SPI.h>');
-  
-  let code = varName + '->begin();\n';
+  let code = '';
+  code += varName + '.begin();\n';
 
   return code;
 }
 
 Arduino.forBlock['esp32_spi_begin_custom'] = function(block, generator) {
-  const varField = block.getField('VAR');
-  const varName = varField ? varField.getText() : 'spi';
+  // 设置变量重命名监听
+  if (!block._spiVarMonitorAttached) {
+    block._spiVarMonitorAttached = true;
+    block._spiVarLastName = block.getFieldValue('VAR') || 'SPI';
+    const varField = block.getField('VAR');
+    if (varField && typeof varField.setValidator === 'function') {
+      varField.setValidator(function(newName) {
+        const workspace = block.workspace || (typeof Blockly !== 'undefined' && Blockly.getMainWorkspace && Blockly.getMainWorkspace());
+        const oldName = block._spiVarLastName;
+        if (workspace && newName && newName !== oldName) {
+          renameVariableInBlockly(block, oldName, newName, 'SPIClass');
+          block._spiVarLastName = newName;
+        }
+        return newName;
+      });
+    }
+  }
+
+  const varName = block.getFieldValue('VAR') || 'SPI';
+  const bus = block.getFieldValue('BUS') || 'HSPI';
   
   const sck = generator.valueToCode(block, 'SCK', generator.ORDER_ATOMIC) || '-1';
   const miso = generator.valueToCode(block, 'MISO', generator.ORDER_ATOMIC) || '-1';
@@ -56,150 +100,162 @@ Arduino.forBlock['esp32_spi_begin_custom'] = function(block, generator) {
 
   generator.addLibrary('SPI', '#include <SPI.h>');
 
-  let code = varName + '->begin(' + sck + ', ' + miso + ', ' + mosi + ', ' + ss + ');\n';
-  
+  // 注册变量到Blockly系统
+  registerVariableToBlockly(varName, 'SPIClass');
+
+  if (bus === 'VSPI') {
+    generator.addVariable(varName, 'SPIClass ' + varName + '(VSPI);');
+  }
+
+  let code = '';
+  code += varName + '.begin(' + sck + ', ' + miso + ', ' + mosi + ', ' + ss + ');\n';
+
   return code;
 };
 
-Arduino.forBlock['esp32_spi_settings'] = function(block, generator) {
-  const varField = block.getField('VAR');
-  const varName = varField ? varField.getText() : 'spi';
+// Arduino.forBlock['esp32_spi_settings'] = function(block, generator) {
+//   const varField = block.getField('VAR');
+//   const varName = varField ? varField.getText() : 'SPI';
   
+//   const frequency = generator.valueToCode(block, 'FREQUENCY', generator.ORDER_ATOMIC) || '1000000';
+//   const bitOrder = block.getFieldValue('BIT_ORDER') || 'MSBFIRST';
+//   const mode = block.getFieldValue('MODE') || '0';
+
+//   generator.addLibrary('SPI', '#include <SPI.h>');
+
+//   let code = '';
+//   code += varName + '.setFrequency(' + frequency + ');\n';
+//   code += varName + '.setBitOrder(' + bitOrder + ');\n';
+//   code += varName + '.setDataMode(' + mode + ');\n';
+  
+//   return code;
+// };
+
+Arduino.forBlock['esp32_spi_begin_transaction'] = function(block, generator) {
+  const varField = block.getField('VAR');
+  const varName = varField ? varField.getText() : 'SPI';
+
+  generator.addLibrary('SPI', '#include <SPI.h>');
+
   const frequency = generator.valueToCode(block, 'FREQUENCY', generator.ORDER_ATOMIC) || '1000000';
   const bitOrder = block.getFieldValue('BIT_ORDER') || 'MSBFIRST';
   const mode = block.getFieldValue('MODE') || '0';
 
-  generator.addLibrary('SPI', '#include <SPI.h>');
-
-  let code = '';
-  code += varName + '->setFrequency(' + frequency + ');\n';
-  code += varName + '->setBitOrder(' + bitOrder + ');\n';
-  code += varName + '->setDataMode(' + mode + ');\n';
-  
-  return code;
-};
-
-Arduino.forBlock['esp32_spi_begin_transaction'] = function(block, generator) {
-  const varField = block.getField('VAR');
-  const varName = varField ? varField.getText() : 'spi';
-
-  generator.addLibrary('SPI', '#include <SPI.h>');
-
   // 使用默认设置开始事务
-  let code = varName + '->beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));\n';
+  let code = varName + '.beginTransaction(SPISettings(' + frequency + ', ' + bitOrder + ', SPI_MODE' + mode + '));\n';
   
   return code;
 };
 
 Arduino.forBlock['esp32_spi_end_transaction'] = function(block, generator) {
   const varField = block.getField('VAR');
-  const varName = varField ? varField.getText() : 'spi';
+  const varName = varField ? varField.getText() : 'SPI';
 
   generator.addLibrary('SPI', '#include <SPI.h>');
 
-  let code = varName + '->endTransaction();\n';
+  let code = varName + '.endTransaction();\n';
   
   return code;
 };
 
 Arduino.forBlock['esp32_spi_transfer'] = function(block, generator) {
   const varField = block.getField('VAR');
-  const varName = varField ? varField.getText() : 'spi';
+  const varName = varField ? varField.getText() : 'SPI';
   
   const data = generator.valueToCode(block, 'DATA', generator.ORDER_ATOMIC) || '0';
 
   generator.addLibrary('SPI', '#include <SPI.h>');
 
-  let code = varName + '->transfer(' + data + ')';
+  let code = varName + '.transfer(' + data + ')';
   
   return [code, generator.ORDER_FUNCTION_CALL];
 };
 
 Arduino.forBlock['esp32_spi_transfer16'] = function(block, generator) {
   const varField = block.getField('VAR');
-  const varName = varField ? varField.getText() : 'spi';
+  const varName = varField ? varField.getText() : 'SPI';
   
   const data = generator.valueToCode(block, 'DATA', generator.ORDER_ATOMIC) || '0';
 
   generator.addLibrary('SPI', '#include <SPI.h>');
 
-  let code = varName + '->transfer16(' + data + ')';
+  let code = varName + '.transfer16(' + data + ')';
   
   return [code, generator.ORDER_FUNCTION_CALL];
 };
 
 Arduino.forBlock['esp32_spi_write'] = function(block, generator) {
   const varField = block.getField('VAR');
-  const varName = varField ? varField.getText() : 'spi';
+  const varName = varField ? varField.getText() : 'SPI';
   
   const data = generator.valueToCode(block, 'DATA', generator.ORDER_ATOMIC) || '0';
 
   generator.addLibrary('SPI', '#include <SPI.h>');
 
-  let code = varName + '->write(' + data + ');\n';
+  let code = varName + '.write(' + data + ');\n';
   
   return code;
 };
 
 Arduino.forBlock['esp32_spi_write_bytes'] = function(block, generator) {
   const varField = block.getField('VAR');
-  const varName = varField ? varField.getText() : 'spi';
+  const varName = varField ? varField.getText() : 'SPI';
   
   const data = generator.valueToCode(block, 'DATA', generator.ORDER_ATOMIC) || 'NULL';
   const length = generator.valueToCode(block, 'LENGTH', generator.ORDER_ATOMIC) || '0';
 
   generator.addLibrary('SPI', '#include <SPI.h>');
 
-  let code = varName + '->writeBytes(' + data + ', ' + length + ');\n';
+  let code = varName + '.writeBytes(' + data + ', ' + length + ');\n';
   
   return code;
 };
 
 Arduino.forBlock['esp32_spi_set_frequency'] = function(block, generator) {
   const varField = block.getField('VAR');
-  const varName = varField ? varField.getText() : 'spi';
+  const varName = varField ? varField.getText() : 'SPI';
   
   const frequency = generator.valueToCode(block, 'FREQUENCY', generator.ORDER_ATOMIC) || '1000000';
 
   generator.addLibrary('SPI', '#include <SPI.h>');
 
-  let code = varName + '->setFrequency(' + frequency + ');\n';
+  let code = varName + '.setFrequency(' + frequency + ');\n';
   
   return code;
 };
 
 Arduino.forBlock['esp32_spi_set_bit_order'] = function(block, generator) {
   const varField = block.getField('VAR');
-  const varName = varField ? varField.getText() : 'spi';
+  const varName = varField ? varField.getText() : 'SPI';
   
   const bitOrder = block.getFieldValue('BIT_ORDER') || 'MSBFIRST';
 
   generator.addLibrary('SPI', '#include <SPI.h>');
 
-  let code = varName + '->setBitOrder(' + bitOrder + ');\n';
+  let code = varName + '.setBitOrder(' + bitOrder + ');\n';
   
   return code;
 };
 
 Arduino.forBlock['esp32_spi_set_data_mode'] = function(block, generator) {
   const varField = block.getField('VAR');
-  const varName = varField ? varField.getText() : 'spi';
+  const varName = varField ? varField.getText() : 'SPI';
   
   const mode = block.getFieldValue('MODE') || '0';
 
   generator.addLibrary('SPI', '#include <SPI.h>');
 
-  let code = varName + '->setDataMode(' + mode + ');\n';
+  let code = varName + '.setDataMode(' + mode + ');\n';
   
   return code;
 };
 
 Arduino.forBlock['esp32_spi_get_ss_pin'] = function(block, generator) {
   const varField = block.getField('VAR');
-  const varName = varField ? varField.getText() : 'spi';
+  const varName = varField ? varField.getText() : 'SPI';
   
   generator.addLibrary('SPI', '#include <SPI.h>');
-  let code = varName + '->pinSS()';
+  let code = varName + '.pinSS()';
   
   return [code, generator.ORDER_FUNCTION_CALL];
 };
--- esp32_spi/readme.md
+++ esp32_spi/readme.md
@@ -1,38 +1,36 @@
-# esp32_spi
+# ESP32 SPI通信库
 
-ESP32 SPI通信操作库
+ESP32平台的SPI通信操作库，支持硬件SPI总线配置、事务控制和数据传输。
 
 ## 库信息
 - **库名**: @aily-project/lib-esp32-spi
 - **版本**: 1.0.0
-- **兼容**: ESP32系列开发板(ESP32/S2/S3/C3/C6/H2)
+- **兼容**: ESP32系列开发板
 
 ## 块定义
 
 | 块类型 | 连接 | 字段/输入 | .abi格式 | 生成代码 |
 |--------|------|----------|----------|----------|
-| `esp32_spi_create` | 语句块 | VAR(field_input), BUS(field_dropdown) | `"fields":{"VAR":"spi","BUS":"VSPI"}` | `SPIClass *spi = new SPIClass(VSPI);` |
-| `esp32_spi_begin_default` | 语句块 | VAR(field_variable) | `"fields":{"VAR":{"id":"spi_id","name":"spi","type":"SPIClass"}}` | `spi->begin();` |
-| `esp32_spi_begin` | 语句块 | VAR(field_variable), SCK/MISO/MOSI/SS(input_value) | `"fields":{"VAR":{"id":"spi_id","name":"spi","type":"SPIClass"}},"inputs":{"SCK":{"block":{"type":"math_number","fields":{"NUM":"18"}}}}` | `spi->begin(18, 19, 23, 5);` |
-| `esp32_spi_settings` | 语句块 | VAR(field_variable), FREQUENCY(input_value), BIT_ORDER/MODE(field_dropdown) | `"fields":{"VAR":{"id":"spi_id","name":"spi","type":"SPIClass"},"BIT_ORDER":"MSBFIRST","MODE":"0"},"inputs":{"FREQUENCY":{"block":{"type":"math_number","fields":{"NUM":"1000000"}}}}` | `spi->setFrequency(1000000); spi->setBitOrder(MSBFIRST);` |
-| `esp32_spi_begin_transaction` | 语句块 | VAR(field_variable) | `"fields":{"VAR":{"id":"spi_id","name":"spi","type":"SPIClass"}}` | `spi->beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));` |
-| `esp32_spi_end_transaction` | 语句块 | VAR(field_variable) | `"fields":{"VAR":{"id":"spi_id","name":"spi","type":"SPIClass"}}` | `spi->endTransaction();` |
-| `esp32_spi_transfer` | 值块 | VAR(field_variable), DATA(input_value) | `"fields":{"VAR":{"id":"spi_id","name":"spi","type":"SPIClass"}},"inputs":{"DATA":{"block":{"type":"math_number","fields":{"NUM":"0"}}}}` | `spi->transfer(0)` |
-| `esp32_spi_transfer16` | 值块 | VAR(field_variable), DATA(input_value) | `"fields":{"VAR":{"id":"spi_id","name":"spi","type":"SPIClass"}},"inputs":{"DATA":{"block":{"type":"math_number","fields":{"NUM":"0"}}}}` | `spi->transfer16(0)` |
-| `esp32_spi_write` | 语句块 | VAR(field_variable), DATA(input_value) | `"fields":{"VAR":{"id":"spi_id","name":"spi","type":"SPIClass"}},"inputs":{"DATA":{"block":{"type":"math_number","fields":{"NUM":"0"}}}}` | `spi->write(0);` |
-| `esp32_spi_write_bytes` | 语句块 | VAR(field_variable), DATA(input_value), LENGTH(input_value) | `"fields":{"VAR":{"id":"spi_id","name":"spi","type":"SPIClass"}},"inputs":{"DATA":{"block":{"type":"variables_get"}},"LENGTH":{"block":{"type":"math_number","fields":{"NUM":"0"}}}}` | `spi->writeBytes(data, 0);` |
-| `esp32_spi_set_frequency` | 语句块 | VAR(field_variable), FREQUENCY(input_value) | `"fields":{"VAR":{"id":"spi_id","name":"spi","type":"SPIClass"}},"inputs":{"FREQUENCY":{"block":{"type":"math_number","fields":{"NUM":"1000000"}}}}` | `spi->setFrequency(1000000);` |
-| `esp32_spi_set_bit_order` | 语句块 | VAR(field_variable), BIT_ORDER(field_dropdown) | `"fields":{"VAR":{"id":"spi_id","name":"spi","type":"SPIClass"},"BIT_ORDER":"MSBFIRST"}` | `spi->setBitOrder(MSBFIRST);` |
-| `esp32_spi_set_data_mode` | 语句块 | VAR(field_variable), MODE(field_dropdown) | `"fields":{"VAR":{"id":"spi_id","name":"spi","type":"SPIClass"},"MODE":"0"}` | `spi->setDataMode(0);` |
-| `esp32_spi_get_ss_pin` | 值块 | VAR(field_variable) | `"fields":{"VAR":{"id":"spi_id","name":"spi","type":"SPIClass"}}` | `spi->pinSS()` |
+| `esp32_spi_begin` | 语句块 | VAR(field_input), BUS(field_dropdown) | `"VAR":"SPI","BUS":"HSPI"` | `SPI.begin();` |
+| `esp32_spi_begin_custom` | 语句块 | VAR(field_input), BUS(field_dropdown), SCK/MISO/MOSI/SS(input_value) | `"VAR":"SPI","BUS":"HSPI"` | `SPI.begin(18,19,23,5);` |
+| `esp32_spi_begin_transaction` | 语句块 | VAR(field_variable), FREQUENCY(input_value), BIT_ORDER/MODE(field_dropdown) | `"VAR":{"id":"spi_id","name":"SPI","type":"SPIClass"},"BIT_ORDER":"MSBFIRST","MODE":"0"` | `SPI.beginTransaction(SPISettings(...));` |
+| `esp32_spi_end_transaction` | 语句块 | VAR(field_variable) | `"VAR":{"id":"spi_id","name":"SPI","type":"SPIClass"}` | `SPI.endTransaction();` |
+| `esp32_spi_transfer` | 值块 | VAR(field_variable), DATA(input_value) | `"VAR":{"id":"spi_id","name":"SPI","type":"SPIClass"}` | `SPI.transfer(data)` |
+| `esp32_spi_transfer16` | 值块 | VAR(field_variable), DATA(input_value) | `"VAR":{"id":"spi_id","name":"SPI","type":"SPIClass"}` | `SPI.transfer16(data)` |
+| `esp32_spi_write` | 语句块 | VAR(field_variable), DATA(input_value) | `"VAR":{"id":"spi_id","name":"SPI","type":"SPIClass"}` | `SPI.write(data);` |
+| `esp32_spi_write_bytes` | 语句块 | VAR(field_variable), DATA/LENGTH(input_value) | `"VAR":{"id":"spi_id","name":"SPI","type":"SPIClass"}` | `SPI.writeBytes(data,len);` |
+| `esp32_spi_set_frequency` | 语句块 | VAR(field_variable), FREQUENCY(input_value) | `"VAR":{"id":"spi_id","name":"SPI","type":"SPIClass"}` | `SPI.setFrequency(freq);` |
+| `esp32_spi_set_bit_order` | 语句块 | VAR(field_variable), BIT_ORDER(field_dropdown) | `"VAR":{"id":"spi_id","name":"SPI","type":"SPIClass"},"BIT_ORDER":"MSBFIRST"` | `SPI.setBitOrder(order);` |
+| `esp32_spi_set_data_mode` | 语句块 | VAR(field_variable), MODE(field_dropdown) | `"VAR":{"id":"spi_id","name":"SPI","type":"SPIClass"},"MODE":"0"` | `SPI.setDataMode(mode);` |
+| `esp32_spi_get_ss_pin` | 值块 | VAR(field_variable) | `"VAR":{"id":"spi_id","name":"SPI","type":"SPIClass"}` | `SPI.pinSS()` |
 
 ## 字段类型映射
 
 | 类型 | .abi格式 | 示例 |
 |------|----------|------|
-| field_input | 字符串 | `"VAR": "spi"` |
-| field_variable | SPIClass变量对象 | `"VAR": {"id": "spi_id", "name": "spi", "type": "SPIClass"}` |
-| field_dropdown | 字符串 | `"BUS": "VSPI"` |
+| field_input | 字符串 | `"VAR": "SPI"` |
+| field_variable | SPIClass变量对象 | `"VAR": {"id": "spi_id", "name": "SPI", "type": "SPIClass"}` |
+| field_dropdown | 字符串 | `"BUS": "HSPI"` |
 | input_value | 块连接 | `"inputs": {"SCK": {"block": {...}}}` |
 
 ## 连接规则
@@ -41,20 +39,20 @@ ESP32 SPI通信操作库
 - **值块**: esp32_spi_transfer、esp32_spi_transfer16、esp32_spi_get_ss_pin有output，连接到`inputs`中
 - **特殊规则**: 
   - SPIClass变量类型为"SPIClass"，每个实例独立管理
-  - esp32_spi_create生成库包含、变量定义和对象创建
+  - 初始化块生成库包含、变量定义和begin调用
   - 初始化块自动添加到setup()区域
   - 支持VSPI和HSPI两个硬件SPI总线
 
 ## 使用示例
 
-### SPI对象创建和初始化
+### SPI默认初始化
 ```json
 {
-  "type": "esp32_spi_create",
+  "type": "esp32_spi_begin",
   "id": "spi_setup",
   "fields": {
-    "VAR": "mySPI",
-    "BUS": "VSPI"
+    "VAR": "SPI",
+    "BUS": "HSPI"
   }
 }
 ```
@@ -62,16 +60,17 @@ ESP32 SPI通信操作库
 ### SPI自定义引脚初始化
 ```json
 {
-  "type": "esp32_spi_begin",
+  "type": "esp32_spi_begin_custom",
   "id": "spi_init_custom",
   "fields": {
-    "VAR": {"id": "spi_var", "name": "mySPI", "type": "SPIClass"}
+    "VAR": "SPI",
+    "BUS": "HSPI"
   },
   "inputs": {
-    "SCK": {"block": {"type": "math_number", "fields": {"NUM": "18"}}},
-    "MISO": {"block": {"type": "math_number", "fields": {"NUM": "19"}}},
-    "MOSI": {"block": {"type": "math_number", "fields": {"NUM": "23"}}},
-    "SS": {"block": {"type": "math_number", "fields": {"NUM": "5"}}}
+    "SCK": {"shadow": {"type": "math_number", "fields": {"NUM": "18"}}},
+    "MISO": {"shadow": {"type": "math_number", "fields": {"NUM": "19"}}},
+    "MOSI": {"shadow": {"type": "math_number", "fields": {"NUM": "23"}}},
+    "SS": {"shadow": {"type": "math_number", "fields": {"NUM": "5"}}}
   }
 }
 ```
@@ -82,25 +81,22 @@ ESP32 SPI通信操作库
   "type": "esp32_spi_begin_transaction",
   "id": "spi_start_trans",
   "fields": {
-    "VAR": {"id": "spi_var", "name": "mySPI", "type": "SPIClass"}
+    "VAR": {"id": "spi_var", "name": "SPI", "type": "SPIClass"},
+    "BIT_ORDER": "MSBFIRST",
+    "MODE": "0"
+  },
+  "inputs": {
+    "FREQUENCY": {"shadow": {"type": "math_number", "fields": {"NUM": "1000000"}}}
   },
   "next": {
     "block": {
-      "type": "variables_set",
-      "fields": {"VAR": {"id": "result_var", "name": "result", "type": "Number"}},
-      "inputs": {
-        "VALUE": {
-          "block": {
-            "type": "esp32_spi_transfer",
-            "fields": {"VAR": {"id": "spi_var", "name": "mySPI", "type": "SPIClass"}},
-            "inputs": {"DATA": {"block": {"type": "math_number", "fields": {"NUM": "0x55"}}}}
-          }
-        }
-      },
+      "type": "esp32_spi_transfer",
+      "fields": {"VAR": {"id": "spi_var", "name": "SPI", "type": "SPIClass"}},
+      "inputs": {"DATA": {"shadow": {"type": "math_number", "fields": {"NUM": "85"}}}},
       "next": {
         "block": {
           "type": "esp32_spi_end_transaction",
-          "fields": {"VAR": {"id": "spi_var", "name": "mySPI", "type": "SPIClass"}}
+          "fields": {"VAR": {"id": "spi_var", "name": "SPI", "type": "SPIClass"}}
         }
       }
     }
@@ -110,24 +106,16 @@ ESP32 SPI通信操作库
 
 ## 重要规则
 
-1. **必须遵守**: SPI对象必须先创建再初始化，事务操作必须配对使用beginTransaction/endTransaction
-2. **连接限制**: esp32_spi_create是语句块必须在setup区域，传输函数是值块返回接收数据
+1. **初始化顺序**: SPI必须先初始化(esp32_spi_begin)再使用，事务操作必须配对使用beginTransaction/endTransaction
+2. **连接限制**: 初始化块是语句块必须在setup区域，传输函数是值块返回接收数据
 3. **变量管理**: 每个SPI实例使用独立的SPIClass类型变量，支持多SPI总线同时使用
-4. **常见错误**: ❌ 未创建对象直接使用，❌ 引脚冲突，❌ 忘记结束事务，❌ 频率设置过高
+4. **常见错误**: ❌ 未初始化直接使用，❌ 引脚冲突，❌ 忘记结束事务，❌ 频率设置过高
 
 ## 支持的字段选项
-- **BUS(SPI总线)**: "VSPI", "HSPI"（ESP32硬件SPI总线）
+- **BUS(SPI总线)**: "HSPI", "VSPI"（ESP32硬件SPI总线）
 - **BIT_ORDER(位序)**: "MSBFIRST"(高位优先), "LSBFIRST"(低位优先)
 - **MODE(通信模式)**: "0"(MODE0), "1"(MODE1), "2"(MODE2), "3"(MODE3)
-- **FREQUENCY(频率范围)**: 80MHz以下（建议1MHz-40MHz）
-- **引脚范围**: GPIO 0-39（避免仅输入引脚34-39作为输出）
 - **变量类型**: "SPIClass"（专用类型，用于ESP32 SPI实例）
 
-## 技术规格
-- **支持平台**: ESP32全系列(ESP32/S2/S3/C3/C6/H2)
-- **硬件总线**: VSPI(SPI3)、HSPI(SPI2)
-- **最大频率**: 80MHz（实际建议40MHz以内）
-- **默认引脚**: VSPI(SCK:18, MISO:19, MOSI:23, SS:5), HSPI(SCK:14, MISO:12, MOSI:13, SS:15)
-- **工作电压**: 3.3V兼容，部分引脚支持5V容忍
-- **传输模式**: 全双工、半双工、单工传输
-- **数据宽度**: 8位、16位数据传输支持
+---
+*自包含文档，无需外部规范*
